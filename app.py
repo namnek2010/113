99930b2d396da332d13022f91c0ca8da363d2b2c
nam2010-pp <nam348tnh@gmail.com>
2025-04-13 02:40:13 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, send_from_directory
+import json
+import os
+
+app = Flask(__name__)
+counter_file = 'counter.json'
+
+def read_counter():
+    if not os.path.exists(counter_file):
+        with open(counter_file, 'w') as f:
+            json.dump({"visits": 0}, f)
+    with open(counter_file, 'r') as f:
+        data = json.load(f)
+    return data["visits"]
+
+def write_counter(count):
+    with open(counter_file, 'w') as f:
+        json.dump({"visits": count}, f)
+
+@app.route('/')
+def index():
+    count = read_counter() + 1
+    write_counter(count)
+    return render_template('index.html', visits=count)
+
+# Route phục vụ file ads.txt
+@app.route('/ads.txt')
+def ads():
+    return send_from_directory('.', 'ads.txt')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2580)
